5c46f3c19367f5406b50baeffc5b678285670e7d
Mironov Andrey <bgmir213@gmail.com>
2016-12-19 21:19:43 +0300

    Диаграммы на главной странице

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <title>Бюджет</title>
 <meta charset="utf-8"/>
 <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/gChartDraw.js"></script>
 <script type="text/javascript" src="js/1.js"></script>
+<script type="text/javascript" src="js/dia.js"></script>
 </head>
 <body onLoad="onLoad()">
 <div id="content">
@@ -20,5 +22,7 @@
 </div>
 	<a href="test/income.php" target="_blank">Диаграмма доходов</a>
 	<a href="test/expense.php" target="_blank">Диаграмма расходов</a>
+	<div id="expenseDia" style="width: 900px; height: 500px;"></div>
+	<div id="incomeDia" style="width: 900px; height: 500px;"></div>
 </body>
 </html>
---
+++ js/dia.js
@@ -0,0 +1,51 @@
+﻿google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+		var options = {
+          pieHole: 0.5,
+          pieSliceTextStyle: {
+            color: 'black',
+          },
+          //legend: 'none'
+        };
+		  
+		var dataDia;
+		var data;
+		$.ajax({
+			async: false,
+			dataType: 'json',
+			url: './ajax/dataSource2.php',
+			success: function(jsondata){
+				dataDia = jsondata;
+			},
+			error: function(){
+				alert('error');
+			}
+		})	  
+
+		data = google.visualization.arrayToDataTable(dataDia);
+
+        var expenseChart = new google.visualization.PieChart(document.getElementById('expenseDia'));
+        expenseChart.draw(data, options);
+		  
+		  
+		// вторая диаграмма
+		  
+		  $.ajax({
+			async: false,
+			dataType: 'json',
+			url: './ajax/dataSource.php',
+			success: function(jsondata){
+				dataDia = jsondata;
+			},
+			error: function(){
+				alert('error');
+			}
+		})	  
+
+		data = google.visualization.arrayToDataTable(dataDia);
+
+        var incomeChart = new google.visualization.PieChart(document.getElementById('incomeDia'));
+        incomeChart.draw(data, options);
+      }
