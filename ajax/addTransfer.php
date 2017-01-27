bb2e4766582c2356be40823424bc3b1286724555
Mironov Andrey <bgmir213@gmail.com>
2017-01-27 19:03:31 +0300

    Реализована транзакционность операций и переводов

--- ajax/addTransfer.php
+++ ajax/addTransfer.php
@@ -3,10 +3,16 @@ require_once 'connectParams.php';
 $mysqli = new mysqli("localhost", $dbUser, $dbPass, $dbName);
 $query = "set names utf8";
 $mysqli->query($query);
+
 $date = $_POST["date"];
 $src = $_POST["src"];
 $dst = $_POST["dst"];
 $amountOfTransfer = $_POST["amountOfTransfer"];
+
+// Начать транзакцию
+$query = "start transaction;";
+$mysqli->query($query);
+
 // Добавить запись о переводе
 $query = "insert into transfers (date, idSrc, idDst, value) values ('".$date."', ".$src.", ".$dst.", ".$amountOfTransfer.")";
 $mysqli->query($query);
@@ -17,6 +23,10 @@ $mysqli->query($query);
 
 // Зачислить средства на счёт - получатель
 $query = "UPDATE accaunts SET balance = balance + $amountOfTransfer WHERE id = ".$dst."";
+$mysqli->query($query);
+
+// Закончить транзакцию
+$query = "commit;";
 $mysqli->query($query);
 
 $mysqli->close();
