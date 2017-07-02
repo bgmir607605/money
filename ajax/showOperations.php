eb5359fb3af357b15575f7c3d36f741f1a71a907
aamir <aamir@home-notebook>
2017-07-02 07:57:27 +0300

    Диаграммы перемещены в модальные окна. Для каждой операции - модальное окно

--- ajax/showOperations.php
+++ ajax/showOperations.php
@@ -12,12 +12,55 @@ if ($month != ''){
 	$condition = $condition.' where month(date) = '.$month.' and year(date) = '.$year;
 }
 
-$query = 'SELECT operations.date, accaunts.name, operations.description, operations.value FROM operations join accaunts on operations.idAccaunt = accaunts.id'.$condition.' order by operations.date' ;
+$query = 'SELECT operations.id, operations.date, accaunts.name, operations.description, operations.value FROM operations join accaunts on operations.idAccaunt = accaunts.id'.$condition.' order by operations.date' ;
 $balance = 0;
 if ($result = $mysqli->query($query)) {
 	while ($row = $result->fetch_assoc()) {
 		$balance = $balance + $row["value"];
-		echo '<div>'.$row["date"].' '.$row["name"].' '.$row["description"].' '.$row["value"].'</div>';
+		
+		
+		echo '<div>'.$row["date"].' '.$row["name"].' '.$row["description"].' '.$row["value"].' ';
+		
+			echo '	<!-- Кнопка, открывающее модальное окно -->
+				<button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#myModal'.$row["id"].'">
+				  <span class="glyphicon glyphicon-cog" title="Редактировать"></span>
+				</button>
+			';
+		echo '</div>';
+		
+		echo '
+			  <!-- Модальное окно -->
+				<div class="modal fade" id="myModal'.$row["id"].'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+				  <div class="modal-dialog" role="document">
+					<div class="modal-content">
+					  <div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
+						  <span aria-hidden="true">×</span>
+						</button>
+						<h4 class="modal-title" id="myModalLabel">Редактирование операции</h4>
+					  </div>
+					  <div class="modal-body">
+						  
+						<div>Дата: '.$row["date"].'<input type="date" class="form-control"></div>
+						<div>Описание: '.$row["description"].'</div>
+						<div>Счёт: '.$row["name"].' <select></select></div>
+						<div>Категория: менять запрос SQL</div>
+						<div>Сумма: '.$row["value"].'</div>
+						<div>'.$row["date"].' '.$row["name"].' '.$row["description"].' '.$row["value"].'</div>
+						
+						
+						
+					  </div>
+					  <div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+						<button class="btn btn-danger"><span class="glyphicon glyphicon-trash"</span> Отменить операцию</button>
+						<button type="button" class="btn btn-primary">Сохранить изменения</button>
+					  </div>
+					</div>
+				  </div>
+				</div>
+		';
+	
 	}
 $result->free();
 }
--- index.php
+++ index.php
@@ -3,16 +3,65 @@
 <head>
 <title>Бюджет</title>
 <meta charset="utf-8"/>
+	<link rel="shortcut icon" href="favicon.ico">
 	<link rel="stylesheet" type="text/css" href="mysite.css">
 <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 <script type="text/javascript" src="js/gChartDraw.js"></script>
 <script type="text/javascript" src="js/dia.js"></script>
 <script type="text/javascript" src="js/1.js"></script>
-</head>
+
+	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	  <link rel="stylesheet" href="css/bootstrap.css">
+<script src="js/bootstrap.js" type="text/javascript"></script>
+	
+	</head>
 <body onLoad="onLoad()">
+	
+	<!-- Кнопка, открывающее модальное окно -->
+
+	  <!-- Модальное окно диаграммы доходов -->
+<div class="modal fade" id="myDiaIn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
+          <span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Доходы за месяц</h4>
+      </div>
+      <div class="modal-body">
+		  <span id="incomeDia"></span>
+      </div>
+    </div>
+  </div>
+</div>
+	
+<!-- Модальное окно диаграммы расходов -->
+<div class="modal fade" id="myDiaOut" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть">
+          <span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Расходы за месяц</h4>
+      </div>
+      <div class="modal-body">
+		<span id="expenseDia"></span>
+      </div>
+    </div>
+  </div>
+</div>
+	
 	<div id="menu">
 		<h3>Меню</h3>
 		<a href="report.php">Отчёты</a>
+		<button type="button" class="btn btn-link" data-toggle="modal" data-target="#myDiaIn">
+			Диаграмма доходов
+		</button>
+		<button type="button" class="btn btn-link" data-toggle="modal" data-target="#myDiaOut">
+			Диаграмма расходов
+		</button>
 	</div>
 <div id="operationsControl">
 	<h3>Ввод операций</h3>
@@ -41,9 +90,10 @@
 	<input type="button" value="Перевести" onClick="addTransfer()">
 </div>
 <div id="transfers"></div>
-<div id="diagrams">
-	<span id="expenseDia"></span>
-	<span id="incomeDia"></span>
-</div>
+
+	
+	
+	
+	
 </body>
 </html>
